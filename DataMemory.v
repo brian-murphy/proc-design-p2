7868c45e8b0a95c6e713b5f1bb510975f3948521
Justin Nicoara <jnicoara11@gmail.com>
2017-11-15 00:13:10 -0500

    Adding my work to github

---
+++ DataMemory.v
@@ -0,0 +1,36 @@
+module DataMemory(clk, addr, dataBus, writeEn);
+	// Basic paremters
+	parameter MEM_INIT_FILE;
+	parameter DMEM_PC_BITS_HI;
+	parameter DMEM_PC_BITS_LO;
+
+	parameter ADDR_BIT_WIDTH = 11;
+	parameter DATA_BIT_WIDTH = 32;
+	parameter N_WORDS = (1 << ADDR_BIT_WIDTH);
+
+	input clk, writeEn;
+	input[DATA_BIT_WIDTH - 1: 0] addr;
+	inout[DATA_BIT_WIDTH - 1: 0] dataBus;
+
+	// Synchronize the address on positive edge clock cycles
+	reg[DATA_BIT_WIDTH - 1: 0] addrReg;
+	always @(negedge clk) addrReg <= addr;
+
+	// Write to the memory
+	always @(negedge clk) begin
+		if (writeEn & isMemEn)
+			data[addr[DMEM_PC_BITS_HI - 1:DMEM_PC_BITS_LO]] <= dataBus;
+	end
+
+	// Determine if the address applies to this module
+	wire isMemEn;
+	assign isMemEn = (addr[DATA_BIT_WIDTH-1:DMEM_PC_BITS_HI] == 0);
+
+	// Initialize the memory
+	(* ram_init_file = MEM_INIT_FILE *)
+	reg[DATA_BIT_WIDTH - 1: 0] data[0: N_WORDS - 1];
+
+	// Write to the bus only if the address corresponds to this module and if the processor does not have writeEn high.
+	assign dataBus = isMemEn & ~writeEn ? data[addrReg[DMEM_PC_BITS_HI-1:DMEM_PC_BITS_LO]]: {DATA_BIT_WIDTH{1'bz}};
+
+endmodule
---
+++ MappedMemoryInputOutput.v
@@ -0,0 +1,23 @@
+module MappedMemoryInputOutput(clk, writeEn, addr, bus, device);
+	// The address of the data register
+	parameter ADDRESS_DATA;
+
+	// The width of the output device
+	parameter OUTPUT_WIDTH;
+
+	input clk, writeEn;
+	input[31:0]	addr;
+	inout[31:0] bus;
+	output reg [OUTPUT_WIDTH-1:0] device;
+
+	// Determine if the address is for this device
+	wire isData;
+	assign isData = addr == ADDRESS_DATA;
+
+	// If reading from the output, place the output on the bus
+	assign bus = (isData&~writeEn)?{{(32-OUTPUT_WIDTH){1'b0}},device}:{32{1'bz}};
+
+	always @(negedge clk)
+		if (isData & writeEn)
+			device <= bus[OUTPUT_WIDTH-1:0];
+endmodule
